ebda1bd023309515b5fff2e2f2b372e7e392dbac
nikita <nikita.comin@gmail.com>
2024-01-27 14:05:04 +0300

    initial

---
+++ app/(auth)/(routs)/layout.tsx
@@ -0,0 +1,13 @@
+const AuthLanding = ({
+    children
+}:{
+    children: React.ReactNode;
+}) =>{
+    return ( 
+        <div className="flex items-center justify-center h-full">
+            {children}
+        </div>
+    );
+}
+
+export default AuthLanding;
